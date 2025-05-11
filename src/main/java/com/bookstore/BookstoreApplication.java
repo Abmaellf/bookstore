c070e5d2df01cad2ad9f834ba8e3ba953db8eb22
Abmaellf <abmael.ninha@gmail.com>
2025-05-11 01:54:04 -0400

    feat(Configurado a conexão com o banco de dados e docker-compose no projeto)

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -2,12 +2,21 @@ package com.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String listMessage() {
+		return "Hello Wold";
 	}
 
 }
